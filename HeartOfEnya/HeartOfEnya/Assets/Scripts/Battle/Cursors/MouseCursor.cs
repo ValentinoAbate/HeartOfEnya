1695e579c90222c31c8c7d279fc32dbc276b06dc
Valentino Abate <valentinoabate1@gmail.com>
2020-02-09 17:44:51 -0800

    Removed keyboard controls from the mouse cursor

--- HeartOfEnya/HeartOfEnya/Assets/Scripts/Battle/Cursors/MouseCursor.cs
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/Battle/Cursors/MouseCursor.cs
@@ -70,27 +70,9 @@ public class MouseCursor : Cursor
     	Select();
     }
 
+    // Now handled by the new input system
     public override void ProcessInput()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Highlight(Pos.Offset(-1, 0));
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            Highlight(Pos.Offset(1, 0));
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            Highlight(Pos.Offset(0, -1));
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            Highlight(Pos.Offset(0, 1));
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Select();
-        }
+
     }
 }

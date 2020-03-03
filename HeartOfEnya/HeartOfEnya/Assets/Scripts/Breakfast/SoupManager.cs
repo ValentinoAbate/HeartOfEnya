f547cb8efabefdcb393824c27d8fcae5e7ca6b95
Benjamin Roisen <broisen@ucsc.edu>
2020-03-02 19:22:08 -0800

    Added persistent-data-based initialization of soup ingredients
    Soup ingredients are now initialized based on drops from the battle stage.
    
    Known bug: potential for deadlock if players collect less than 3 ingredients in battle. This will be fixed in the next push.

--- HeartOfEnya/HeartOfEnya/Assets/Scripts/Breakfast/SoupManager.cs
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/Breakfast/SoupManager.cs
@@ -49,25 +49,59 @@ public class SoupManager : MonoBehaviour
     /// </summary>
     private void Initialize()
     {
-    	//spawn all enabled buttons
-    	int totalSpawned = 0; //how many buttons we've currently spawned
-    	for (int i = 0; i < totalIngredients; i++)
-    	{
-    		if (enabledIngredients[i])
-    		{
-    			//instantiate a button at the next location
-    			var button = Instantiate(buttonPrefab);
-    			//connect it to the UI canvas and move it to the next open slot
-    			button.transform.SetParent(parentCanvas.transform, false);
-    			button.transform.localPosition = new Vector3(buttonX, buttonY - (totalSpawned * buttonOffset), 0);
-    			//set its ingredient & ID
-    			button.ingredient = ingredients[i];
-    			button.SetID(i); //use i instead of totalSpawned so that we can use the ID as an index to retrieve its ingredient from the ingredients list
-    			button.UpdateData(); //tell the button to refresh its images with data from the new ingredient
+        //figure out which ingredients are enabled
+        List<string> gatheredIngredients = DoNotDestroyOnLoad.Instance.persistentData.gatheredIngredients;
+        Debug.Log("***GATHERED " + gatheredIngredients.Count + " INGREDIENTS:***");
+        foreach (string x in gatheredIngredients)
+        {
+            Debug.Log(x);
+        }
+        Debug.Log("***END GATHERED***");
 
-    			totalSpawned++;
-    		}
+        //Iterate through the master ingredients list, checking if each one was enabled.
+    	//It's an O(n^2) monstrosity, but it's okay because there's only a few ingredients.
+        //Plus it helps prevent duplicates by checking each unique ingredient only once.
+        int totalSpawned = 0; //how many buttons we've currently spawned
+        for (int i = 0; i < totalIngredients; i++)
+    	{
+            //get an ingredient from the master list, then check if it's present in the gatheredIngredients list
+            Ingredient ing = ingredients[i]; //current ingredient from the master list
+            if (gatheredIngredients.Contains(ing.name))
+            {
+                //ingredient was found last round, and should be enabled
+                Debug.Log("Enabling ingredient: " + ing.name);
+                //gatheredIngredients.Remove(ing.name); //minor optimization; also used to ID any ingredients that don't get found
+
+                //spawn the button
+                var button = Instantiate(buttonPrefab);
+                //connect it to the UI canvas and move it to the next open slot
+                button.transform.SetParent(parentCanvas.transform, false);
+                button.transform.localPosition = new Vector3(buttonX, buttonY - (totalSpawned * buttonOffset), 0);
+                //set its ingredient & ID
+                button.ingredient = ing;
+                button.SetID(i); //use i instead of totalSpawned so that we can use the ID as an index to retrieve its ingredient from the ingredients list
+                button.UpdateData(); //tell the button to refresh its images with data from the new ingredient
+
+                totalSpawned++;
+            }
+            else
+            {
+                //ingredient was not found last round, and thus will not be enabled
+                Debug.Log("Ingredient " + ing.name + " isn't enabled");
+            }
     	}
+
+        //debug cleanup
+        // if (gatheredIngredients.Count > 0)
+        // {
+        //     Debug.Log("No ingredients found for: " + gatheredIngredients);
+        // }
+
+        //sanity check - if fewer than 3 buttons were spawned...
+        if (totalSpawned < 3)
+        {
+            //
+        }
     }
 
     /// <summary>

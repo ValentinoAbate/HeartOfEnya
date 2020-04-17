ef3216816854b103b9829459796d639e7de33ab7
Benjamin Roisen <broisen@ucsc.edu>
2020-04-17 15:15:27 -0700

    Started work on updated MoveEffect
    MoveEffect now detects being pushed into world border and deals damage accordingly. However, the task is still WIP and will be further developed shortly w/ chained movement, etc.

--- HeartOfEnya/HeartOfEnya/Assets/Scripts/Battle/Actions/MoveEffect.cs
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/Battle/Actions/MoveEffect.cs
@@ -12,15 +12,54 @@ public class MoveEffect : ActionEffect
     }
     public Direction moveType;
     public int squares = 1;
+    public int moveDamage = 2; //how much damage do we take if pushed into world border/immovable object?
 
     public override IEnumerator ApplyEffect(Combatant user, Combatant target)
     {
         Pos direction = Pos.DirectionBasic(user.Pos, target.Pos);
         if (moveType == Direction.Toward || (moveType == Direction.Hybrid && Pos.Distance(user.Pos, target.Pos) > 1))
             direction *= -1;
-        BattleGrid.main.MoveAndSetWorldPos(target, target.Pos + direction);
-        var src = GetComponent<AudioSource>();
-        src.PlayOneShot(target.moveSfx);
-        yield return new WaitForSeconds(target.moveSfx.length);
+        Pos destination = target.Pos + direction; //where are we trying to push the object to?
+        FieldObject thingAtDestination = BattleGrid.main.GetObject(destination); //what is in our way?
+        Debug.Log("moving: " + thingAtDestination);
+
+        //what do we do with thingAtDestination?
+        if (!BattleGrid.main.IsLegal(destination)) //destination is a world border
+        {
+        	Debug.Log("cannot move into world border, so get smote");
+        	//copied from DamageEffect
+        	Debug.Log(user.DisplayName + " dealt " + moveDamage + " damage to " + target.DisplayName + " with " + name);
+        	bool death = moveDamage >= target.Hp;
+        	var src = GetComponent<AudioSource>();
+        	target.Damage(moveDamage);
+        	src.PlayOneShot(target.damageSfx);
+        	yield return new WaitForSeconds(target.damageSfx.length);
+        	if (death)
+        	{
+        		src.PlayOneShot(target.deathSfx);
+        		yield return new WaitForSeconds(target.deathSfx.length);
+        	}
+        }
+        else if (thingAtDestination == null) //destination is an empty square
+        {
+        	Debug.Log("moving into empty square");
+        	BattleGrid.main.MoveAndSetWorldPos(target, destination);
+        	var src = GetComponent<AudioSource>();
+        	src.PlayOneShot(target.moveSfx);
+        	yield return new WaitForSeconds(target.moveSfx.length);
+        }
+        else
+        {
+        	//for now, just do what we were doing before until I add an isMovable variable to obstacles
+        	BattleGrid.main.MoveAndSetWorldPos(target, destination);
+        	var src = GetComponent<AudioSource>();
+        	src.PlayOneShot(target.moveSfx);
+        	yield return new WaitForSeconds(target.moveSfx.length);
+        }
+
+        // BattleGrid.main.MoveAndSetWorldPos(target, destination);
+        // var src = GetComponent<AudioSource>();
+        // src.PlayOneShot(target.moveSfx);
+        // yield return new WaitForSeconds(target.moveSfx.length);
     }
 }

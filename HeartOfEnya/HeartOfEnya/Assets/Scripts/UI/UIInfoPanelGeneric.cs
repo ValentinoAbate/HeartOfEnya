84b50825eb70439803ca0e09d42ab21b7060b836
Valentino Abate <valentinoabate1@gmail.com>
2020-06-01 14:53:22 -0700

    Obstacle info panel new layout
    - Corrected descriptions on snowbank prefabs, and properly set big snowbank's HP to 3.

--- HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/UIInfoPanelGeneric.cs
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/UIInfoPanelGeneric.cs
@@ -7,23 +7,24 @@ using TMPro;
 public class UIInfoPanelGeneric : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
-    public Image unitImage;
     public TextMeshProUGUI descriptionText;
-    public TextMeshProUGUI statusText;
+    public TextMeshProUGUI unitPropertiesText;
+    public TextMeshProUGUI[] effectTexts = new TextMeshProUGUI[4];
     public TextMeshProUGUI moveNumberText;
     public TextMeshProUGUI hpNumberText;
+    public TextMeshProUGUI damageNumberText;
     public Image hpBarImage;
-    public void ShowUI(Combatant c)
+
+    public void ShowUI(Combatant e)
     {
-        nameText.text = c.DisplayName;
-        descriptionText.text = c.description;
-        statusText.text = c.IsChargingAction ? "Status: Charging" : "Status: Normal";
-        if (c.Stunned)
-            statusText.text = "Status: Stunned";
-        moveNumberText.text = c.Move.ToString();
-        hpNumberText.text = c.Hp.ToString();
-        hpBarImage.fillAmount = c.Hp / (float)c.maxHp;
-        unitImage.sprite = c.DisplaySprite;
-        unitImage.color = c.DisplaySpriteColor;
+        nameText.text = e.DisplayName;
+        descriptionText.text = e.description;
+        moveNumberText.text = e.Move.ToString();
+        hpNumberText.text = e.Hp.ToString();
+        descriptionText.text = e.description;
+        unitPropertiesText.text = e.isMovable ? string.Empty : "Immovable";
+        damageNumberText.text = "N/A";
+        effectTexts[0].text = "No Actions";
+
     }
 }

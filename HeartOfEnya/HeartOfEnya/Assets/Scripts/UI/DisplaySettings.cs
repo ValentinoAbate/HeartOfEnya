d8e9e41e1b2d5c058e9efc9453f7416381dbad93
Valentino Abate <valentinoabate1@gmail.com>
2020-06-04 16:37:52 -0700

    Options Menu Integration, Puasing Fixes
    - Dialog boxes properly pause in all scenes
    - Still progress when the pause button is clicked
    - Settings menu accessible from main menu and pause menu from all scenes

---
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/DisplaySettings.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaySettings : MonoBehaviour
+{
+    private FullScreenMode mode;
+    private int resolutionW = 1920;
+    private int resolutionH = 1080;
+    public void SetFullScreenModeWindowed()
+    {
+        mode = FullScreenMode.Windowed;
+        Apply();
+    }
+
+    public void SetFullScreenModeWindowedMax()
+    {
+        mode = FullScreenMode.MaximizedWindow;
+        Apply();
+    }
+
+    public void SetFullScreenModeFullScreen()
+    {
+        mode = FullScreenMode.FullScreenWindow;
+        Apply();
+    }
+
+    public void SetResolutionWidth(int w) => resolutionW = w;
+    public void SetResolutionHeight(int h) => resolutionH = h;
+
+    public void Apply()
+    {
+        Screen.SetResolution(resolutionW, resolutionH, mode);
+    }
+}

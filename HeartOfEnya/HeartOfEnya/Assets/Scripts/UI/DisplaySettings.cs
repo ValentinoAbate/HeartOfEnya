ddc4b17b9dbf73f55649c3f9f3bad1f2506b6494
Valentino Abate <valentinoabate1@gmail.com>
2020-06-04 18:21:55 -0700

    Options Menu now can use dropdown menus
    - the dropdown menus are unskinned, but can be edited through the OptionsDropdown prefab.
    - If you want to use the dropdowns, please disable all relevant display and resolution buttons.
    - Apply button needs to stay regardless of which method is used!

--- HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/DisplaySettings.cs
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/DisplaySettings.cs
@@ -3,16 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.Linq;
 
 public class DisplaySettings : MonoBehaviour
 {
-    private FullScreenMode mode;
+    public FullScreenMode mode;
     private Resolution resolution;
     public TextMeshProUGUI selectorText;
+    public TMP_Dropdown displayDropDown;
+    public TMP_Dropdown resolutionDropDown;
 
     private void Awake()
     {
         resolution = Screen.currentResolution;
+        resolutionDropDown.value = Array.IndexOf(Screen.resolutions.Where((r) => r.width / 16 == r.height / 9).ToArray(), resolution);
+        mode = Screen.fullScreenMode;
+        displayDropDown.value = (int)mode;
         selectorText.text = resolution.ToString();
     }
 
@@ -36,11 +42,22 @@ public class DisplaySettings : MonoBehaviour
 
     public void NextResolution()
     {
-        int ind = Array.IndexOf(Screen.resolutions, resolution);
+        int ind = Array.IndexOf(Screen.resolutions.Where((r) => r.width / 16 == r.height / 9).ToArray(), resolution);
         if(++ind >= Screen.resolutions.Length)
             ind = 0;
             resolution = Screen.resolutions[ind];
         selectorText.text = resolution.ToString();
+    }
+
+    public void SetResolution(int ind)
+    {
+        resolution = Screen.resolutions[ind];
+        selectorText.text = resolution.ToString();
+    }
+
+    public void SetFullScreenMode(int mode)
+    {
+        this.mode = (FullScreenMode)mode;
     }
 
     public void Apply()

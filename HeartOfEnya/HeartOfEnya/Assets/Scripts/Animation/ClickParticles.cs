330e98997bef049c4e23c5737da68c6e68febd3b
Gigi Bachtel <gbachtel@ucsc.edu>
2020-06-06 15:48:19 -0700

    Credits scene WIP
    Still need
    - special thanks (fill out or else :owogun: )
    - BGM (suggestions?)
    - return to title on finish

---
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/Animation/ClickParticles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickParticles : MonoBehaviour
+{
+    [SerializeField]
+    GameObject psObject;
+
+    [SerializeField]
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) PlayClickParticles();
+    }
+
+    public void PlayClickParticles()
+    {
+       
+       Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+       pos = new Vector3(pos.x, pos.y, 0);
+       Instantiate(psObject, pos, new Quaternion());
+    }
+}
---
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/Animation/CreditsAnimation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsAnimation : MonoBehaviour
+{
+    RectTransform tf;
+
+    [SerializeField]
+    Animator logoanim;
+
+    [SerializeField]
+    RectTransform endobj;
+
+    [SerializeField]
+    float scrollSpeed;
+
+    bool started = false;
+    bool finished = false;
+
+    void Start()
+    {
+        tf = gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(started)
+        {
+            if(!finished)
+            {
+                ScrollCredits();
+            }
+            else
+            {
+                Debug.Log("Credits finished");
+            }
+        }
+        else if(!finished)
+        {
+            if (logoanim.GetCurrentAnimatorStateInfo(0).IsName("Logo Startup") &&
+                logoanim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.5f)
+            {
+                Debug.Log("Logo finished");
+                started = true;
+                StartCoroutine(SpeedUp(scrollSpeed));
+            }
+        }
+    }
+
+    void ScrollCredits()
+    {
+        float speed = (Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0)) ? scrollSpeed * 2 : scrollSpeed;
+        tf.anchoredPosition = new Vector2(tf.anchoredPosition.x,
+                                          tf.anchoredPosition.y + Time.deltaTime * speed);
+        if (endobj.TransformPoint(endobj.localPosition).y > 0) finished = true;
+    }
+
+    IEnumerator SpeedUp(float topspeed)
+    {
+        float stepcount = 10;
+        for(int i = 0; i < stepcount; i++)
+        {
+            scrollSpeed = Mathf.SmoothStep(0, topspeed, i / stepcount);
+            yield return new WaitForSeconds(1.5f / stepcount);
+        }
+        scrollSpeed = topspeed;
+    }
+}

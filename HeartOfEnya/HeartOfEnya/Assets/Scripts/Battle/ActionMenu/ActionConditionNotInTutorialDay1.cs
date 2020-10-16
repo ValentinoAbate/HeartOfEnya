2a2ea930a4ba278be5f082d291fc68e96e8baa32
Valentino Abate <valentinoabate1@gmail.com>
2020-10-15 17:20:27 -0700

    Bapy Can No Longer Push / Pull on Tutorial Day 1
    - Prevents softlock with spawn covering and makes more sense from a design / narrative point of view

---
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/Battle/ActionMenu/ActionConditionNotInTutorialDay1.cs
@@ -0,0 +1,8 @@
+﻿public class ActionConditionNotInTutorialDay1 : ActionCondition
+{
+    protected override bool CheckConditionFn(ActionMenu menu, PartyMember user)
+    {
+        var pData = DoNotDestroyOnLoad.Instance.persistentData;
+        return !pData.InTutorialFirstDay;
+    }
+}

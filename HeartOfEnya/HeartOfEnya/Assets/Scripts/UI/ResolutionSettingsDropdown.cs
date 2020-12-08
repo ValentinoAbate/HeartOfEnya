6bf435430c436ded86212b13d65f542c59493557
Benjamin Roisen <ben1roisen@gmail.com>
2020-12-08 03:00:27 -0800

    WIP on limiting resolutions to native resolution or smaller
    Added a way to (theoretically) find the native resolution and remove all resolutions larger than it.
    
    WARNING: this hasn't been tested on a system with the XL resolutions bug. Do not merge this branch until I've confirmed it fixes the error!

--- HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/ResolutionSettingsDropdown.cs
+++ HeartOfEnya/HeartOfEnya/Assets/Scripts/UI/ResolutionSettingsDropdown.cs
@@ -13,9 +13,70 @@ public class ResolutionSettingsDropdown : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        menu.AddOptions(Screen.resolutions.Select((r) => new TMP_Dropdown.OptionData(r.ToString())).ToList());
-        menu.value = Array.IndexOf(Screen.resolutions, Screen.currentResolution);
-        label.text = Screen.resolutions[menu.value].ToString();
-        menu.onValueChanged.AddListener((val) => label.text = Screen.resolutions[val].ToString());
+        //generate a list of resolutions that are smaller or equal to the native resolution
+        Resolution[] availableResolutions = getLegalResolutions();
+
+        menu.AddOptions(availableResolutions.Select((r) => new TMP_Dropdown.OptionData(r.ToString())).ToList());
+        menu.value = Array.IndexOf(availableResolutions, Screen.currentResolution);
+        //in the unlikely event we somehow removed the current resolution from the list, scream bloody murder
+        if (Array.Exists(availableResolutions, element => isResEqual(element, Screen.currentResolution))) //all is fine
+        {
+            label.text = availableResolutions[menu.value].ToString();
+        }
+        else //current resolution somehow got removed
+        {
+            Debug.LogError("Current resolution is not in the list of available resolutions!");
+            label.text = "!!UNKNOWN RESOLUTION!!"; //since I can only properly test this system in builds (where the console doesn't show), this is the easiest way to indicate this error in-game.
+        }
+        //label.text = availableResolutions[menu.value].ToString();
+        menu.onValueChanged.AddListener((val) => label.text = availableResolutions[val].ToString());
+    }
+
+    //goes through the list of available resolutions and removes any larger than the native resolution
+    Resolution[] getLegalResolutions()
+    {
+        //get list of all available resolutions
+        Resolution[] availableRes = Screen.resolutions;
+
+        //Get the native resolution so we can remove everything above it.
+        //We'll use the systemWidth/systemHeight properties of the Display class, since unlike Screen.currentResolution they remain constant regardless of any changes to the display or resolution settings.
+        Resolution native = new Resolution();       //This refuses to compile if fed arguments and I can't find any documentation of a constructor...
+        native.width = Display.main.systemWidth;    //..so we'll just use the default 0x0 @ 0Hz and manually adjust the width and height.
+        native.height = Display.main.systemHeight;  //We can leave the refresh rate at 0, since we're not filtering based on it (and the native refresh rate is harder to find).
+        Debug.Log("NATIVE RES: " + native.ToString());
+
+        //go through the available resolutions and toss out everything higher than the native resolution
+        //Debug.Log("PRE-FILTERING:");
+        //printResArray(availableRes);
+        availableRes = Array.FindAll(availableRes, x => (x.width <= native.width && x.height <= native.height));
+        //Debug.Log("POST-FILTERING:");
+        //printResArray(availableRes);
+
+        return availableRes;
+    }
+
+    //helper method for debugging, prints the contents of an array of Resolutions
+    void printResArray(Resolution[] arr)
+    {
+        string output = "";
+        foreach (Resolution r in arr)
+        {
+            output += r.ToString();
+            output += "\n";
+        }
+        Debug.Log(output);
+    }
+
+    //helper method for testing Resolution equality
+    bool isResEqual(Resolution a, Resolution b)
+    {
+        if ((a.width == b.width) && (a.height == b.height) && (a.refreshRate == b.refreshRate))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
